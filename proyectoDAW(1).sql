6b1be4b633464e58e65f042ba5ae4fd2df8fbd68
Jose Fons <josefoons@gmail.com>
2019-01-22 19:56:06 +0100

    addd db

---
+++ proyectoDAW(1).sql
@@ -0,0 +1,571 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 22, 2019 at 07:55 PM
+-- Server version: 5.7.24-0ubuntu0.18.04.1
+-- PHP Version: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `proyectoDAW`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elo`
+--
+
+CREATE TABLE `elo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(64) NOT NULL,
+  `nombreArchivo` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `idioma`
+--
+
+CREATE TABLE `idioma` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` char(49) CHARACTER SET utf8 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `idioma`
+--
+
+INSERT INTO `idioma` (`id`, `nombre`) VALUES
+(1, 'English'),
+(2, 'Afar'),
+(3, 'Abkhazian'),
+(4, 'Afrikaans'),
+(5, 'Amharic'),
+(6, 'Arabic'),
+(7, 'Assamese'),
+(8, 'Aymara'),
+(9, 'Azerbaijani'),
+(10, 'Bashkir'),
+(11, 'Belarusian'),
+(12, 'Bulgarian'),
+(13, 'Bihari'),
+(14, 'Bislama'),
+(15, 'Bengali/Bangla'),
+(16, 'Tibetan'),
+(17, 'Breton'),
+(18, 'Catalan'),
+(19, 'Corsican'),
+(20, 'Czech'),
+(21, 'Welsh'),
+(22, 'Danish'),
+(23, 'German'),
+(24, 'Bhutani'),
+(25, 'Greek'),
+(26, 'Esperanto'),
+(27, 'Spanish'),
+(28, 'Estonian'),
+(29, 'Basque'),
+(30, 'Persian'),
+(31, 'Finnish'),
+(32, 'Fiji'),
+(33, 'Faeroese'),
+(34, 'French'),
+(35, 'Frisian'),
+(36, 'Irish'),
+(37, 'Scots/Gaelic'),
+(38, 'Galician'),
+(39, 'Guarani'),
+(40, 'Gujarati'),
+(41, 'Hausa'),
+(42, 'Hindi'),
+(43, 'Croatian'),
+(44, 'Hungarian'),
+(45, 'Armenian'),
+(46, 'Interlingua'),
+(47, 'Interlingue'),
+(48, 'Inupiak'),
+(49, 'Indonesian'),
+(50, 'Icelandic'),
+(51, 'Italian'),
+(52, 'Hebrew'),
+(53, 'Japanese'),
+(54, 'Yiddish'),
+(55, 'Javanese'),
+(56, 'Georgian'),
+(57, 'Kazakh'),
+(58, 'Greenlandic'),
+(59, 'Cambodian'),
+(60, 'Kannada'),
+(61, 'Korean'),
+(62, 'Kashmiri'),
+(63, 'Kurdish'),
+(64, 'Kirghiz'),
+(65, 'Latin'),
+(66, 'Lingala'),
+(67, 'Laothian'),
+(68, 'Lithuanian'),
+(69, 'Latvian/Lettish'),
+(70, 'Malagasy'),
+(71, 'Maori'),
+(72, 'Macedonian'),
+(73, 'Malayalam'),
+(74, 'Mongolian'),
+(75, 'Moldavian'),
+(76, 'Marathi'),
+(77, 'Malay'),
+(78, 'Maltese'),
+(79, 'Burmese'),
+(80, 'Nauru'),
+(81, 'Nepali'),
+(82, 'Dutch'),
+(83, 'Norwegian'),
+(84, 'Occitan'),
+(85, '(Afan)/Oromoor/Oriya'),
+(86, 'Punjabi'),
+(87, 'Polish'),
+(88, 'Pashto/Pushto'),
+(89, 'Portuguese'),
+(90, 'Quechua'),
+(91, 'Rhaeto-Romance'),
+(92, 'Kirundi'),
+(93, 'Romanian'),
+(94, 'Russian'),
+(95, 'Kinyarwanda'),
+(96, 'Sanskrit'),
+(97, 'Sindhi'),
+(98, 'Sangro'),
+(99, 'Serbo-Croatian'),
+(100, 'Singhalese'),
+(101, 'Slovak'),
+(102, 'Slovenian'),
+(103, 'Samoan'),
+(104, 'Shona'),
+(105, 'Somali'),
+(106, 'Albanian'),
+(107, 'Serbian'),
+(108, 'Siswati'),
+(109, 'Sesotho'),
+(110, 'Sundanese'),
+(111, 'Swedish'),
+(112, 'Swahili'),
+(113, 'Tamil'),
+(114, 'Telugu'),
+(115, 'Tajik'),
+(116, 'Thai'),
+(117, 'Tigrinya'),
+(118, 'Turkmen'),
+(119, 'Tagalog'),
+(120, 'Setswana'),
+(121, 'Tonga'),
+(122, 'Turkish'),
+(123, 'Tsonga'),
+(124, 'Tatar'),
+(125, 'Twi'),
+(126, 'Ukrainian'),
+(127, 'Urdu'),
+(128, 'Uzbek'),
+(129, 'Vietnamese'),
+(130, 'Volapuk'),
+(131, 'Wolof'),
+(132, 'Xhosa'),
+(133, 'Yoruba'),
+(134, 'Chinese'),
+(135, 'Zulu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pais`
+--
+
+CREATE TABLE `pais` (
+  `id` int(5) NOT NULL,
+  `nombre` varchar(45) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pais`
+--
+
+INSERT INTO `pais` (`id`, `nombre`) VALUES
+(1, 'Andorra'),
+(2, 'United Arab Emirates'),
+(3, 'Afghanistan'),
+(4, 'Antigua and Barbuda'),
+(5, 'Anguilla'),
+(6, 'Albania'),
+(7, 'Armenia'),
+(8, 'Angola'),
+(9, 'Antarctica'),
+(10, 'Argentina'),
+(11, 'American Samoa'),
+(12, 'Austria'),
+(13, 'Australia'),
+(14, 'Aruba'),
+(15, 'Åland'),
+(16, 'Azerbaijan'),
+(17, 'Bosnia and Herzegovina'),
+(18, 'Barbados'),
+(19, 'Bangladesh'),
+(20, 'Belgium'),
+(21, 'Burkina Faso'),
+(22, 'Bulgaria'),
+(23, 'Bahrain'),
+(24, 'Burundi'),
+(25, 'Benin'),
+(26, 'Saint Barthélemy'),
+(27, 'Bermuda'),
+(28, 'Brunei'),
+(29, 'Bolivia'),
+(30, 'Bonaire'),
+(31, 'Brazil'),
+(32, 'Bahamas'),
+(33, 'Bhutan'),
+(34, 'Bouvet Island'),
+(35, 'Botswana'),
+(36, 'Belarus'),
+(37, 'Belize'),
+(38, 'Canada'),
+(39, 'Cocos [Keeling] Islands'),
+(40, 'Democratic Republic of the Congo'),
+(41, 'Central African Republic'),
+(42, 'Republic of the Congo'),
+(43, 'Switzerland'),
+(44, 'Ivory Coast'),
+(45, 'Cook Islands'),
+(46, 'Chile'),
+(47, 'Cameroon'),
+(48, 'China'),
+(49, 'Colombia'),
+(50, 'Costa Rica'),
+(51, 'Cuba'),
+(52, 'Cape Verde'),
+(53, 'Curacao'),
+(54, 'Christmas Island'),
+(55, 'Cyprus'),
+(56, 'Czechia'),
+(57, 'Germany'),
+(58, 'Djibouti'),
+(59, 'Denmark'),
+(60, 'Dominica'),
+(61, 'Dominican Republic'),
+(62, 'Algeria'),
+(63, 'Ecuador'),
+(64, 'Estonia'),
+(65, 'Egypt'),
+(66, 'Western Sahara'),
+(67, 'Eritrea'),
+(68, 'Spain'),
+(69, 'Ethiopia'),
+(70, 'Finland'),
+(71, 'Fiji'),
+(72, 'Falkland Islands'),
+(73, 'Micronesia'),
+(74, 'Faroe Islands'),
+(75, 'France'),
+(76, 'Gabon'),
+(77, 'United Kingdom'),
+(78, 'Grenada'),
+(79, 'Georgia'),
+(80, 'French Guiana'),
+(81, 'Guernsey'),
+(82, 'Ghana'),
+(83, 'Gibraltar'),
+(84, 'Greenland'),
+(85, 'Gambia'),
+(86, 'Guinea'),
+(87, 'Guadeloupe'),
+(88, 'Equatorial Guinea'),
+(89, 'Greece'),
+(90, 'South Georgia and the South Sandwich Islands'),
+(91, 'Guatemala'),
+(92, 'Guam'),
+(93, 'Guinea-Bissau'),
+(94, 'Guyana'),
+(95, 'Hong Kong'),
+(96, 'Heard Island and McDonald Islands'),
+(97, 'Honduras'),
+(98, 'Croatia'),
+(99, 'Haiti'),
+(100, 'Hungary'),
+(101, 'Indonesia'),
+(102, 'Ireland'),
+(103, 'Israel'),
+(104, 'Isle of Man'),
+(105, 'India'),
+(106, 'British Indian Ocean Territory'),
+(107, 'Iraq'),
+(108, 'Iran'),
+(109, 'Iceland'),
+(110, 'Italy'),
+(111, 'Jersey'),
+(112, 'Jamaica'),
+(113, 'Jordan'),
+(114, 'Japan'),
+(115, 'Kenya'),
+(116, 'Kyrgyzstan'),
+(117, 'Cambodia'),
+(118, 'Kiribati'),
+(119, 'Comoros'),
+(120, 'Saint Kitts and Nevis'),
+(121, 'North Korea'),
+(122, 'South Korea'),
+(123, 'Kuwait'),
+(124, 'Cayman Islands'),
+(125, 'Kazakhstan'),
+(126, 'Laos'),
+(127, 'Lebanon'),
+(128, 'Saint Lucia'),
+(129, 'Liechtenstein'),
+(130, 'Sri Lanka'),
+(131, 'Liberia'),
+(132, 'Lesotho'),
+(133, 'Lithuania'),
+(134, 'Luxembourg'),
+(135, 'Latvia'),
+(136, 'Libya'),
+(137, 'Morocco'),
+(138, 'Monaco'),
+(139, 'Moldova'),
+(140, 'Montenegro'),
+(141, 'Saint Martin'),
+(142, 'Madagascar'),
+(143, 'Marshall Islands'),
+(144, 'Macedonia'),
+(145, 'Mali'),
+(146, 'Myanmar [Burma]'),
+(147, 'Mongolia'),
+(148, 'Macao'),
+(149, 'Northern Mariana Islands'),
+(150, 'Martinique'),
+(151, 'Mauritania'),
+(152, 'Montserrat'),
+(153, 'Malta'),
+(154, 'Mauritius'),
+(155, 'Maldives'),
+(156, 'Malawi'),
+(157, 'Mexico'),
+(158, 'Malaysia'),
+(159, 'Mozambique'),
+(160, 'Namibia'),
+(161, 'New Caledonia'),
+(162, 'Niger'),
+(163, 'Norfolk Island'),
+(164, 'Nigeria'),
+(165, 'Nicaragua'),
+(166, 'Netherlands'),
+(167, 'Norway'),
+(168, 'Nepal'),
+(169, 'Nauru'),
+(170, 'Niue'),
+(171, 'New Zealand'),
+(172, 'Oman'),
+(173, 'Panama'),
+(174, 'Peru'),
+(175, 'French Polynesia'),
+(176, 'Papua New Guinea'),
+(177, 'Philippines'),
+(178, 'Pakistan'),
+(179, 'Poland'),
+(180, 'Saint Pierre and Miquelon'),
+(181, 'Pitcairn Islands'),
+(182, 'Puerto Rico'),
+(183, 'Palestine'),
+(184, 'Portugal'),
+(185, 'Palau'),
+(186, 'Paraguay'),
+(187, 'Qatar'),
+(188, 'Réunion'),
+(189, 'Romania'),
+(190, 'Serbia'),
+(191, 'Russia'),
+(192, 'Rwanda'),
+(193, 'Saudi Arabia'),
+(194, 'Solomon Islands'),
+(195, 'Seychelles'),
+(196, 'Sudan'),
+(197, 'Sweden'),
+(198, 'Singapore'),
+(199, 'Saint Helena'),
+(200, 'Slovenia'),
+(201, 'Svalbard and Jan Mayen'),
+(202, 'Slovakia'),
+(203, 'Sierra Leone'),
+(204, 'San Marino'),
+(205, 'Senegal'),
+(206, 'Somalia'),
+(207, 'Suriname'),
+(208, 'South Sudan'),
+(209, 'São Tomé and Príncipe'),
+(210, 'El Salvador'),
+(211, 'Sint Maarten'),
+(212, 'Syria'),
+(213, 'Swaziland'),
+(214, 'Turks and Caicos Islands'),
+(215, 'Chad'),
+(216, 'French Southern Territories'),
+(217, 'Togo'),
+(218, 'Thailand'),
+(219, 'Tajikistan'),
+(220, 'Tokelau'),
+(221, 'East Timor'),
+(222, 'Turkmenistan'),
+(223, 'Tunisia'),
+(224, 'Tonga'),
+(225, 'Turkey'),
+(226, 'Trinidad and Tobago'),
+(227, 'Tuvalu'),
+(228, 'Taiwan'),
+(229, 'Tanzania'),
+(230, 'Ukraine'),
+(231, 'Uganda'),
+(232, 'U.S. Minor Outlying Islands'),
+(233, 'United States'),
+(234, 'Uruguay'),
+(235, 'Uzbekistan'),
+(236, 'Vatican City'),
+(237, 'Saint Vincent and the Grenadines'),
+(238, 'Venezuela'),
+(239, 'British Virgin Islands'),
+(240, 'U.S. Virgin Islands'),
+(241, 'Vietnam'),
+(242, 'Vanuatu'),
+(243, 'Wallis and Futuna'),
+(244, 'Samoa'),
+(245, 'Kosovo'),
+(246, 'Yemen'),
+(247, 'Mayotte'),
+(248, 'South Africa'),
+(249, 'Zambia'),
+(250, 'Zimbabwe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `region`
+--
+
+CREATE TABLE `region` (
+  `id` int(5) NOT NULL,
+  `codigoRegion` char(8) NOT NULL DEFAULT '',
+  `nombreRegion` varchar(45) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `region`
+--
+
+INSERT INTO `region` (`id`, `codigoRegion`, `nombreRegion`) VALUES
+(1, 'BR', 'Brasil'),
+(2, 'EUNE', 'Europa Este'),
+(3, 'EUW', 'Europa Oeste'),
+(4, 'LAN', 'Latinoamerica Norte'),
+(5, 'LAS', 'Latinoamerica Sur'),
+(6, 'NA', 'America del Norte'),
+(7, 'OCE', 'Oceania'),
+(8, 'RU', 'Rusia'),
+(9, 'TR', 'Turquia'),
+(10, 'JP', 'Japon'),
+(11, 'KR', 'Corea'),
+(12, 'CN', 'China'),
+(13, 'SEA', 'Sudeste Asia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nick` varchar(64) NOT NULL,
+  `password` binary(16) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `idPais` int(11) NOT NULL,
+  `idIdioma` int(11) NOT NULL,
+  `idElo` int(11) NOT NULL,
+  `rolPreferido` varchar(64) NOT NULL,
+  `rolBuscado` varchar(64) NOT NULL,
+  `idRegion` int(11) NOT NULL,
+  `mensaje` text NOT NULL,
+  `rolWeb` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `elo`
+--
+ALTER TABLE `elo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `idioma`
+--
+ALTER TABLE `idioma`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `region`
+--
+ALTER TABLE `region`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `elo`
+--
+ALTER TABLE `elo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `idioma`
+--
+ALTER TABLE `idioma`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+--
+-- AUTO_INCREMENT for table `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=251;
+--
+-- AUTO_INCREMENT for table `region`
+--
+ALTER TABLE `region`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
