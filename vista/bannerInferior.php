1164dcb13413bdb64911c058812906fa0937283d
Jose Fons <josefoons@gmail.com>
2019-02-05 18:59:20 +0100

    Apuntes

--- vista/bannerInferior.php
+++ vista/bannerInferior.php
@@ -1,17 +1,23 @@
-<hr>
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Siguenos en Twitter</h5>
-            <p class="card-text">Enterate de Sorteos</p>
-            <a href="#" class="btn btn-primary">TWITTER</a>
-          </div>
+<br>
+  <div class="row">
+    <!-- BANNER IZQUIERDA -->
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Siguenos en Twitter</h5>
+          <p class="card-text">Enterate de Sorteos</p>
+          <a href="#" class="btn btn-primary">TWITTER</a>
         </div>
       </div>
-      <div class="col-sm-6" id="contenedorBanner">
-        <img src="vista/img/banner_inferior.jpg" id="bannerHeimer">
+      <!-- /BANNER IZQUIERDA -->
+      <!-- BANNER DERECHA -->
       </div>
+        <div class="col-sm-6" id="contenedorBanner">
+          <img src="vista/img/banner_inferior.jpg" id="bannerHeimer">
+        </div>
+      </div>
+      <!-- /BANNER DERECHA -->
     </div>
-    <hr>
   </div>
+</div>
+<br>
