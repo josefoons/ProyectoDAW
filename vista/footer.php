927dcbbb5aab43a3bcd5a182076152566a75469f
Jose Fons <josefoons@gmail.com>
2019-02-12 15:55:11 +0100

    Subir footer

--- vista/footer.php
+++ vista/footer.php
@@ -1,7 +1,36 @@
-<footer class="py-5 bg-dark">
+<!-- <footer class="py-5 bg-dark">
     <div class="container">
       <p class="m-0 text-center text-white">Copyright &copy; Jose Fons</p>
     </div>
-  </footer>
+  </footer> -->
+  <footer id="myFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="#"> <img src="vista/img/logo.png" /> </a></h2>
+                </div>
+                <div class="col-sm-5">
+                  <center>
+                    <h5>Enlaces Utiles</h5>
+                      <ul>
+                          <li><a href="index.php">Home</a></li>
+                          <li><a href="registro.php">Registro</a></li>
+                          <li><a href="login.php">Login</a></li>
+                      </ul>
+                    </center>
+                </div>
+                <div class="col-sm-3">
+                    <div class="social-networks">
+                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="https://github.com/josefoons/ProyectoDAW" target="_blank" class="github"><i class="fa fa-github-square"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2019 Copyright Jose Fons </p>
+        </div>
+    </footer>
 </body>
 </html>
--- vista/header.php
+++ vista/header.php
@@ -20,6 +20,7 @@
     crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="vista/css/custom.css" rel="stylesheet">
+<link href="vista/css/footer.css" rel="stylesheet">
 <script src="vista/js/ip.js"></script>
 
 <?php
