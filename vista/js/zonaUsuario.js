dadabf157316dbae6666d8ceb90ebf3b583866da
Jose Fons <josefoons@gmail.com>
2019-02-18 19:53:21 +0100

    ups

--- vista/js/zonaUsuario.js
+++ vista/js/zonaUsuario.js
@@ -243,8 +243,7 @@ function crearReporte() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            //location.reload();
+            location.reload();
         }
     };
 
